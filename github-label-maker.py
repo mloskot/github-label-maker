7999502c7665b2b5774643ed568489288b817ad4
Mateusz Łoskot <mateusz@loskot.net>
2018-06-27 22:20:31 +0200

    WIP

--- github-label-maker.py
+++ github-label-maker.py
@@ -4,86 +4,13 @@
 #
 # This is free and unencumbered software released into the public domain.
 #
-from binascii import crc32
 import argparse
-import json
 import logging
 import os
 import sys
-import github
+import glm
 
 log = logging.getLogger('glm')
-
-class GitHub:
-    def __init__(self, github_token, github_owner_name, github_repo_name):
-        assert isinstance(github_owner_name, str)
-        assert isinstance(github_repo_name, str)
-
-        g = github.Github(github_token)
-        assert g.get_user().login
-
-        log.info("authorized to github as user '%s'", g.get_user().login)
-        rate_limit = g.get_rate_limit()
-        log.info('rate limit=%d, remaining=%d', rate_limit.rate.limit, rate_limit.rate.remaining)
-
-        # Repository either owned by user or one of user's organization
-        orgs = [org.login for org in g.get_user().get_orgs()]
-        if github_owner_name in orgs:
-            owner = g.get_organization(github_owner_name)
-        else:
-            owner = g.get_user()
-        self.repo = owner.get_repo(github_repo_name)
-        log.info("connected to repository '%s/%s'", owner.login, self.repo.name)
-
-    def add_label(self, name, color, description=None):
-        if color.startswith('#'):
-            color = color[1:]
-        if not description:
-            description = github.GithubObject.NotSet
-        log.info("creating label '%s'", name)
-        return self.repo.create_label(name, color, description)
-
-    def add_labels(self, labels):
-        for label in labels:
-            assert 'name' in label
-            assert 'color' in label
-            description = None
-            if 'description' in label:
-                description = label['description']
-            self.add_label(label['name'], label['color'], description)
-
-    def delete_labels(self):
-        for label in self.repo.get_labels():
-            log.info('deleting label %s', label.name)
-            label.delete()
-
-    def dump_labels(self):
-        labels = []
-        repo_labels = self.repo.get_labels()
-        for label in repo_labels:
-            entry = { "name" : label.name, "color": "#{0}".format(label.color) }
-            if label.description is not github.GithubObject.NotSet:
-                entry['description'] = label.description
-            labels.append(entry)
-        return labels
-
-def make(github_token, github_owner, github_repo, labels_from, append_mode=False):
-    hub = GitHub(github_token, github_owner, github_repo)
-    if not append_mode:
-        hub.delete_labels()
-    for _, labels_file in labels_from.items():
-        log.info("creating labels from '%s'", labels_file)
-        with open(labels_file, 'r') as f:
-            labels_def = json.load(f)
-            hub.add_labels(labels_def)
-
-def dump(github_token, github_owner, github_repo, labels_to):
-    hub = GitHub(github_token, github_owner, github_repo)
-    labels_def = hub.dump_labels()
-    log.info("dumping labels to '%s'", labels_to)
-    with open(labels_to, 'w') as f:
-        labels_def = json.dumps(labels_def, indent=2)
-        f.write(labels_def)
 
 if __name__ == "__main__":
     p = argparse.ArgumentParser(description='Make GitHub labels from definitions in labels/*.json or restore GitHub defaults from labels/default.json')
@@ -109,7 +36,7 @@ if __name__ == "__main__":
     if args.dump_labels_to:
         dump_file = args.dump_labels_to
         assert dump_file.endswith('json')
-        dump(args.token, args.owner, args.repository, dump_file)
+        glm.dump(args.token, args.owner, args.repository, dump_file)
     else:
         if os.path.isdir(args.make_labels_from):
             labels_dir = args.make_labels_from
@@ -124,4 +51,4 @@ if __name__ == "__main__":
             labels_files = {
                 os.path.splitext(f)[0]: args.make_labels_from
             }
-        make(args.token, args.owner, args.repository, labels_files, args.make_append)
+        glm.make(args.token, args.owner, args.repository, labels_files, args.make_append)
---
+++ test_glm.py
@@ -0,0 +1,9 @@
+import unittest
+import glm
+
+class GithubLaberMakerTest(unittest.TestCase):
+    def setUp(self):
+        self.setUp()
+
+    def test_clean(self):
+        glm.clear()
